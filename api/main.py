540423f28849cde39a5d07d84bd91997a9e1207f
dindonpingpong <d_nguen@mail.ru>
2022-05-21 23:08:34 +0300

    Persist storage

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import psycopg2
 
 app = FastAPI()
 
@@ -18,6 +19,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+URI = "postgres://dhrmhtfbhqplfe:1ad5bfc52102553cd11f612e44f6fa0f626ee7f16bd3a4ee3c7e7712677dbf87@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/d5nnlffhse9ujv"
+# Connect to your postgres DB
+conn = psycopg2.connect(URI)
+
 class User(BaseModel):
     name: str
     riddle: str
@@ -30,10 +35,36 @@ def read_root():
 
 @app.get("/users")
 def get_users():
-    return [{ "name": key, "riddle": users[key] } for key in users]
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users")
+
+    records = cur.fetchall()
+
+    return [{ "name": user[1], "riddle": user[2] } for user in records]
 
 @app.post("/users")
 def add_user(user: User):
-    users[user.name.lower()] = user.riddle
+    cur = conn.cursor()
 
-    return [{ "name": key, "riddle": users[key] } for key in users]
+    query = """ INSERT INTO users (nickname, riddle)
+                    VALUES (%s, %s)
+                    ON CONFLICT (nickname) DO UPDATE
+                    SET riddle = %s"""
+
+    record_to_insert = (user.name, user.riddle, user.riddle)
+
+    cur.execute(query, record_to_insert)
+
+    conn.commit()
+
+    count = cur.rowcount
+
+    if count == 0:
+        return []
+
+    cur.execute("SELECT * FROM users")
+
+    records = cur.fetchall()
+
+    return [{ "name": user[1], "riddle": user[2] } for user in records]
---
+++ api/migrations/users.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+  id serial,
+  login text,
+  riddle text
+)
